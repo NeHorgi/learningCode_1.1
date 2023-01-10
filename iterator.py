96ea1297a01d63bc742d1cf4e4d7a13e84592ac1
NeHorgi <119290694+NeHorgi@users.noreply.github.com>
2023-01-10 20:45:38 +0100

    Add files via upload

---
+++ iterator.py
@@ -0,0 +1,34 @@
+'''
+Реализуйте итератор колоды карт (52 штуки) CardDeck
+Каждая карта представлена в виде строки типа "2 Пик". При вызове функции next()
+Будет возвращаться следующая карта. "3 Пик "...
+По окончании перебора всех элементов выводится сообщения "Колода закончилась"
+'''
+
+
+class CardDeck:
+
+    def __init__(self):
+        self.Deck = 52
+        self.card = 0
+        self.colors = ['Пик', 'Черви', 'Буби', 'Крести']
+        self.cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Валет', 'Дама', 'Король', 'Туз']
+
+    def __next__(self):
+
+        while self.card < self.Deck:
+
+            for color in self.colors:
+                for card in self.cards:
+                    self.card += 1
+                    print(f'{card} {color}')
+
+        print(f'Колода закончилась')
+        StopIteration
+
+    def __iter__(self):
+        return self
+
+if __name__ == '__main__':
+    deck = CardDeck()
+    deck.__next__()
